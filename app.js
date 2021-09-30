5da3e3fc9193ed11e208efee26320ac667fd6443
James Demiraiakian <james.armen@gmail.com>
2021-09-30 15:34:15 -0700

    refactored to use single input/out text fields for simplicity. Added multiplication buttting in HTML

--- app.js
+++ app.js
@@ -3,29 +3,25 @@ import { add, sub } from './calculations.js';
 // import functions and grab DOM elements
 
 //Add
-const addInput1 = document.getElementById('add-input1');
-const addInput2 = document.getElementById('add-input2');
-const addOutput = document.getElementById('add-output');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const output = document.getElementById('output');
 const addButton = document.getElementById('add-button');
-
-//Sub
-const subInput1 = document.getElementById('sub-input1');
-const subInput2 = document.getElementById('sub-input2');
-const subOutput = document.getElementById('sub-output');
 const subButton = document.getElementById('sub-button');
+
 
 //console.log(addInput1, addInput2, addOutput, addButton);
 
 addButton.addEventListener('click', () => {
-    const value1 = Number(addInput1.value);
-    const value2 = Number(addInput2.value);
+    const value1 = Number(input1.value);
+    const value2 = Number(input2.value);
     const result = add(value1, value2);
-    addOutput.textContent = result;
+    output.textContent = result;
 });
 
 subButton.addEventListener('click', () => {
-    const value1 = Number(subInput1.value);
-    const value2 = Number(subInput2.value);
+    const value1 = Number(input1.value);
+    const value2 = Number(input2.value);
     const result = sub(value1, value2);
-    subOutput.textContent = result;
+    output.textContent = result;
 });
